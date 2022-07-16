5871255a6adbe898bb5d606a65ec9d853654a46e
kevin breid <kevinbreid@yahoo.com>
2022-07-15 19:01:49 -0700

    commit tree b77afc866fcb8552556a9699ea9af7bf13330f9c

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  for(let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      console.log('This is i and j:', i, j)
+    if(array[i] + array[j] === target){
+      return true
+
+    }
+  }
+
+} return false
 }
+
+// function hasTargetSum(nums, target) {
+
+//   let n = nums.length;
+  
+//   for(i=0;i<n;i++) {
+//     for(j=i+1;j<n;j++) {
+	
+//       if(nums[i]+nums[j]==target) {
+//         // out.push(i,j);
+
+//         return true
+//       }
+//     }
+//   }  return false;
+// };
+
+// hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+
+// 3, 8, 12, 4, 11, 7
+// 0  1   2  3  4   5
+
+// 3, 8, 12, 4, 11, 7
+// 0  1   2  3  4   5
+
+
+// sumNum =  10
+// if i + j = sumNum, return true
+
 
 /* 
   Write the Big O time complexity of your function here
